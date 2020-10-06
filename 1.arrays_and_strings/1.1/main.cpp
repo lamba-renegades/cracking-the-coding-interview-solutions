97c768ca5763b177f7236403887b1672285cdc5d
ChristianTovar <tovarchristian21@gmail.com>
2020-10-06 10:51:41 -0500

    Add solution 1.1 C++

---
+++ 1.arrays_and_strings/1.1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+bool isUnique(string s) {
+  if (s == "") return true;
+
+  sort(s.begin(), s.end());
+
+  for (string::size_type i = 0; i < s.size() - 1; i++) {
+    if (s[i] == s[i + 1]) return false;
+  }
+
+  return true;
+}
+
+int main() {
+  cout << isUnique("qwerty"); // 1
+  cout << isUnique(""); // 1
+  cout << isUnique("sdfghiojvci"); // 0
+  cout << isUnique("qwertyuiopasdfghjklzxcvbnm"); // 1
+  cout << isUnique("cvnbofoqaolo"); // 0
+}
